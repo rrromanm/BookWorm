cc4a9cfb64b41d888596dab5e8277ff17cb638fc
xvvcs <maciek.m2004@wp.pl>
2024-04-23 18:32:39 +0200

    fixed table in manage accounts

--- src/main/java/sep/view/AdminManageAccountsViewController.java
+++ src/main/java/sep/view/AdminManageAccountsViewController.java
@@ -1,8 +1,14 @@
 package sep.view;
 
+import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Region;
+import sep.model.Book;
+import sep.model.User;
 import sep.viewmodel.AdminManageAccountsViewModel;
 import sep.viewmodel.DonateViewModel;
 
@@ -18,11 +24,31 @@ public class AdminManageAccountsViewController {
     private Button DeleteButton;
     @FXML
     private Button SaveUserButton;
+    @FXML
+    private TableView<User> UserTableView;
+    @FXML private TableColumn<User, Integer> IDcolumn;
+    @FXML private TableColumn<User, String> FirstNameColumn;
+    @FXML private TableColumn<User, String> LastNameColumn;
+    @FXML private TableColumn<User, String> UsernameColumn;
+    @FXML private TableColumn<User, String> EmailColumn;
+    @FXML private TableColumn<User, Integer> phoneNumberColumn;
+    private ReadOnlyObjectProperty<User> selectedUser;
 
     public void init(ViewHandler viewHandler, AdminManageAccountsViewModel viewModel, Region root) {
         this.viewHandler = viewHandler;
         this.viewModel = viewModel;
         this.root = root;
+        initializeTableView();
+        this.selectedUser = UserTableView.getSelectionModel().selectedItemProperty();
+    }
+
+    public void initializeTableView() {
+        IDcolumn.setCellValueFactory(new PropertyValueFactory<>("userID"));
+        FirstNameColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        LastNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        UsernameColumn.setCellValueFactory(new PropertyValueFactory<>("username"));
+        EmailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
+        phoneNumberColumn.setCellValueFactory(new PropertyValueFactory<>("phoneNumber"));
     }
     @FXML
     private void backButtonClicked(){
